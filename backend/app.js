c80cb4c615166371df0e6a06375cfcaabb297244
munirudreamer <munirudreamer@gmail.com>
2025-05-14 17:56:30 +0000

    install and created express server

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const db = require('../config/db')
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Ghana Project Tracker API');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+
+// var mongoURL = 'mongodb+srv://Dreamer:Dreamer2223@cluster0.mg1um.mongodb.net/mern-pizza'
+var mongoURL = 'mongodb+srv://maher:4422@cluster0.mg1um.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+// var mongoURL = 'mongodb+srv://Dreamer:Dreamer2223@cluster0.mg1um.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(mongoURL, {useUnifiedTopology: true, useNewUrlParser: true})
+
+let db = mongoose.connection
+//check connection{
+    db.on('open', function(){
+        console.log('Connected to MongoDB')
+    });
+//check for db errors
+db.on('error', function(err){
+    console.log(err)
+})
+
+
+module.exports = mongoose
