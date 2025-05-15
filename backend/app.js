92bab9613297d8cf6f133f3f5a51dde04d39a6ba
munirudreamer <munirudreamer@gmail.com>
2025-05-15 19:53:22 +0000

    installed nodemon and had server running

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,15 @@
 // app.js
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('../config/db')
-const app = express();
+
+const db = require('./')
+
+const app = express('./config/db.js');
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// Database connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.get('/', (req, res) => {
---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+// config/db.js
+const mongoose = require('mongoose');
+
+const mongoURL = 'mongodb+srv://maher:4422@cluster0.mg1um.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(mongoURL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
+
+const db = mongoose.connection;
+
+db.on('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+module.exports = db;
