1044563a2ac3e1f60961d3fe66101bf8366b95c3
munirudreamer <munirudreamer@gmail.com>
2025-06-02 22:39:41 +0000

    removed the project list file

--- frontend/src/components/ProjectList.jsx
+++ frontend/src/components/ProjectList.jsx
@@ -1,28 +1,28 @@
-// src/components/ProjectList.jsx
-import React, { useEffect, useState } from 'react';
-import { fetchProjects } from '../services/api';
+// // src/components/ProjectList.jsx
+// import React, { useEffect, useState } from 'react';
+// import { fetchProjects } from '../services/api';
 
-const ProjectList = () => {
-  const [projects, setProjects] = useState([]);
+// const ProjectList = () => {
+//   const [projects, setProjects] = useState([]);
 
-  useEffect(() => {
-    fetchProjects()
-      .then((res) => setProjects(res.data.projects))
-      .catch((err) => console.error(err));
-  }, []);
+//   useEffect(() => {
+//     fetchProjects()
+//       .then((res) => setProjects(res.data.projects))
+//       .catch((err) => console.error(err));
+//   }, []);
 
-  return (
-    <div>
-      <h2>Onboarded Projects</h2>
-      <ul>
-        {projects.map((project) => (
-          <li key={project._id}>
-            <strong>{project.title}</strong> – {project.district}, {project.region}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>Onboarded Projects</h2>
+//       <ul>
+//         {projects.map((project) => (
+//           <li key={project._id}>
+//             <strong>{project.title}</strong> – {project.district}, {project.region}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default ProjectList;
+// export default ProjectList;
