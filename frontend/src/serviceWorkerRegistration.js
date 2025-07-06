ce2e24c2a5141cf4df685e6f94723bee73d3d462
munirudreamer <munirudreamer@gmail.com>
2025-07-06 17:11:05 +0000

    started to implement PWA

---
+++ frontend/src/serviceWorkerRegistration.js
@@ -0,0 +1,25 @@
+// Based on Create React App's serviceWorker setup
+
+// Check if browser supports service workers
+export function register() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => {
+          console.log('Service Worker registered: ', registration);
+        })
+        .catch((registrationError) => {
+          console.log('Service Worker registration failed: ', registrationError);
+        });
+    });
+  }
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then((registration) => {
+      registration.unregister();
+    });
+  }
+}
